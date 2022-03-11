a5c3c8e45f7111d2a7d27ec1ec9018df6ab8f848
Diego Cuenca <dcuenca@lorente.fin.ec>
2022-03-11 16:58:47 -0500

    Utilizando vistas basadas en clases

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 # We don't necessarily need to add these extra url patterns in, 
