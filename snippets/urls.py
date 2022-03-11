7aaaa145885687bf3442952b7235c99fe12438a2
Diego Cuenca <dcuenca@lorente.fin.ec>
2022-03-11 17:54:37 -0500

    Vistas genéricas basadas en clases

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/', views.SnippetListGen.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetailGen.as_view()),
 ]
 
 # We don't necessarily need to add these extra url patterns in, 
--- snippets/views.py
+++ snippets/views.py
@@ -3,9 +3,24 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+
+# Vistas Genéricas basadas en clases
+
+class SnippetListGen(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetailGen(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+# Vistas basadas en clases
 
 class SnippetList(APIView):
     '''
@@ -55,7 +70,9 @@ class SnippetDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])  #Vistas basadas en funciones
+# Vistas basadas en funciones
+
+@api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
     '''
     List all code snippets, or create a new snippet.
@@ -72,6 +89,7 @@ def snippet_list(request, format=None):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def snippet_detail(request, pk, format=None):
